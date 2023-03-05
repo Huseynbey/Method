daf08f17f616c1cb2882d2661251a3cb1567da23
Huseyn Baymammadli <huseynnb@code.edu.az>
2023-03-05 16:30:31 +0400

    Add project files.

---
+++ Method/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1
+            //Console.WriteLine("Enter Number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(AddSeven(num));
+
+            //Q2
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //SumValuable(number);
+
+
+        }
+        //static int AddSeven(int num)
+        //{
+        //    if (num % 7 == 0)
+        //    {
+        //        return num;
+        //    }
+        //    else if (num < 7)
+        //    {
+        //        num = 7;
+        //        return num;
+        //    }
+        //    else if (num % 7 < 4)
+        //    {
+        //        return num / 7 * 7;
+        //    }
+        //    else
+        //    {
+        //        return ((num + 4) / 7) * 7;
+        //    }
+
+        //}
+        //static void SumValuable(int number)
+        //{
+
+        //    int sum = 0;
+
+        //    for (int i = 0; i < number; i++)
+        //    {
+        //        sum += i;
+        //    }
+        //    Console.WriteLine(sum);
+        }
+    }
+}
